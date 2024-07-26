11d9dcbc5a00da07ed2ad595c216a1541b9ff82e
dvankevich <dvankevich@gmail.com>
2024-07-26 19:08:57 +0000

    Deploying to gh-pages from @ dvankevich/goit-js-hw-09@d78ee8ff7985c3ec66a3b3e548d42f93687ce836 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/styles-4ed993c7.js";console.log("1-gallery.js");
+//# sourceMappingURL=commonHelpers.js.map
---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/styles-4ed993c7.js";console.log("2-form.js");
+//# sourceMappingURL=commonHelpers2.js.map
