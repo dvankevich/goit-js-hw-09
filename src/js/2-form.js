8ef6e873a3ce24e8867b229a209ba9a0f31ac975
dvankevich <dvankevich@gmail.com>
2024-07-28 12:35:36 +0300

    write form script

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,53 @@
-console.log('2-form.js');
+'use strict';
+
+let formData = JSON.parse('{"email":"","message":""}');
+
+const form = document.querySelector('.feedback-form');
+const email = form.elements.email;
+const message = form.elements.message;
+const localStorageKey = 'feedback-form-state';
+
+//formData = localStorage.getItem(localStorageKey);
+
+// https://builtin.com/software-engineering-perspectives/javascript-null-check
+
+if (Object.is(localStorage.getItem(localStorageKey), null)) {
+  formData = JSON.parse('{"email":"","message":""}');
+  //console.log(formData);
+} else {
+  formData = JSON.parse(localStorage.getItem(localStorageKey));
+  //console.log(formData);
+}
+
+email.value = formData.email;
+message.value = formData.message;
+
+form.addEventListener('input', evt => {
+  //console.log(evt.target.attributes.name.value);
+  switch (evt.target.attributes.name.value) {
+    case 'email':
+      formData.email = evt.target.value;
+      break;
+    case 'message':
+      formData.message = evt.target.value;
+      break;
+  }
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  //console.log(evt.target.elements.email.value);
+  if (
+    evt.target.elements.message.value === '' ||
+    evt.target.elements.email.value === ''
+  ) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+  }
+});
+
+//console.log(JSON.stringify(localStorage.getItem(localStorageKey)));
